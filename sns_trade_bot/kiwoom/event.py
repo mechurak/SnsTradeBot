e5720b56a91009307497c32314a7228e0e94301b
sshim from series9 <mechurak@gmail.com>
2020-03-25 00:50:22 +0900

    Add Fid enum

--- sns_trade_bot/kiwoom/event.py
+++ sns_trade_bot/kiwoom/event.py
@@ -2,7 +2,7 @@ import logging
 
 from sns_trade_bot.model.data_manager import DataManager, DataType
 from sns_trade_bot.model.stock import Stock
-from sns_trade_bot.kiwoom.common import ScreenNo, RqName, EventHandler, TrResultKey
+from sns_trade_bot.kiwoom.common import ScreenNo, RqName, EventHandler, TrResultKey, Fid
 from sns_trade_bot.kiwoom.internal import KiwoomOcx
 
 logger = logging.getLogger(__name__)
@@ -101,6 +101,10 @@ class KiwoomEventHandler(EventHandler):
                 self.data_manager.set_updated(DataType.TABLE_BALANCE)
             else:
                 logger.error("error!!")
+        elif rq_name == RqName.ORDER.value:
+            order_id = self.ocx.get_comm_data(tr_code, record_name, 0, '종목코드').strip()
+            logger.info(f'order_id:{order_id}')
+            # TODO: Handle error case (empty order_id)
 
     def on_receive_real_data(self, code: str, real_type: str, real_data: str):
         logger.debug(f"code:{code} real_type:{real_type} real_data:{real_data}")
@@ -108,7 +112,7 @@ class KiwoomEventHandler(EventHandler):
             # TODO: Trigger time related strategy
             pass
         elif real_type == '주식체결':
-            price_str = self.ocx.get_comm_real_data(code, 10)
+            price_str = self.ocx.get_comm_real_data(code, Fid.현재가.value)
             cur_price = int(price_str)
             cur_price = cur_price if cur_price >= 0 else cur_price * (-1)
 
@@ -124,7 +128,7 @@ class KiwoomEventHandler(EventHandler):
     def on_receive_msg(self, scr_no: str, rq_name: str, tr_code: str, msg: str):
         logger.info(f'scr_no:{scr_no}, rq_name:{rq_name}, tr_code:{tr_code}, msg:{msg}')
 
-    def on_receive_chejan_data(self, gubun, item_cnt, fid_list):
+    def on_receive_chejan_data(self, gubun: str, item_cnt: int, fid_list: str):
         """체결 데이터를 받은 시점을 알려준다
 
         :param gubun: 체결구분 (0:접수/체결 , 1:국내주식 잔고통보, 4:파생상품 잔고통보)
@@ -134,29 +138,37 @@ class KiwoomEventHandler(EventHandler):
         logger.info(f'gubun:{gubun}, item_cnt:{item_cnt}, fid_list:{fid_list}')
         fid_str_list = fid_list.split(";")
         for fid_str in fid_str_list:
-            ret = self.ocx.get_chejan_data(int(fid_str))
-            logger.debug(f'  {fid_str}: {ret}')
+            fid = int(fid_str)
+            ret = self.ocx.get_chejan_data(fid)
+            key_str = f'{Fid(fid).name}({fid})' if fid in Fid.__members__.values() else f'{fid_str}'
+            logger.debug(f'  {key_str}: "{ret}"')
 
-        order_id = self.ocx.get_chejan_data(9203)  # 주문번호
-        code = self.ocx.get_chejan_data(9001)  # 종목코드,업종코드
+        order_id = self.ocx.get_chejan_data(Fid.주문번호.value)
+        code = self.ocx.get_chejan_data(Fid.종목코드.value)
         code = code[1:]  # Remove 'A'
-        name = self.ocx.get_chejan_data(302)  # 종목명
+        name = self.ocx.get_chejan_data(Fid.종목명.value)
+
+        logger.info('temp')
 
         if gubun == '0':  # 주문접수 or 주문체결
-            status = self.ocx.get_chejan_data(913)  # 주문상태 ('접수' or '체결')
-            order_type = self.ocx.get_chejan_data(913)  # 매도수구분 ('1':매도, '2':매수)
-            time_str = self.ocx.get_chejan_data(908)  # 주문/체결시간 (HHMMSSMS)
+            status = self.ocx.get_chejan_data(Fid.주문상태.value)  # '접수' or '체결'
+            order_type = self.ocx.get_chejan_data(Fid.매도수구분.value)  # '1':매도, '2':매수
+            time_str = self.ocx.get_chejan_data(Fid.주문_체결시간.value)  # 주문/체결시간 (HHMMSSMS)
             if status == '체결':
                 logger.info('체결 신호!!!')
                 if order_type == '2':  # 매수
                     # TODO: 종목 실시간 등록 및 조건식 실시간 재적용(?)
                     pass
         elif gubun == '1':  # 잔고통보
-            qty = int(self.ocx.get_chejan_data(930))  # 보유수량
+            qty = int(self.ocx.get_chejan_data(Fid.보유수량.value))
             if qty == 0:
                 logger.info(f'{name}({code}) 청산 완료!!')
                 self.data_manager.remove_stock(code)
                 self.ocx.set_real_remove(ScreenNo.REAL.value, code)
+            else:
+                logger.info(f'{name}({code}) qty:{qty}')
+                stock = self.data_manager.get_stock(code)
+                stock.qty = qty
 
     def on_receive_condition_ver(self, ret: int, msg: str):
         logger.debug(f'ret: {ret}, msg: {msg}')  # ret: 사용자 조건식 저장 성공여부 (1: 성공, 나머지 실패)
---
+++ test/kiwoom/test_kiwoom_event_chejan.py
@@ -0,0 +1,97 @@
+import logging
+import sys
+import unittest
+from unittest.mock import Mock
+
+from PyQt5.QtWidgets import *
+from sns_trade_bot.kiwoom.manager import Kiwoom
+from sns_trade_bot.kiwoom.common import ScreenNo, RqName
+from sns_trade_bot.model.data_manager import DataManager, ModelListener, DataType
+
+logger = logging.getLogger()
+logger.level = logging.DEBUG
+if not logger.hasHandlers():
+    stream_handler = logging.StreamHandler(sys.stdout)
+    f = logging.Formatter('%(asctime)s[%(levelname)8s](%(filename)20s:%(lineno)-4s %(funcName)-35s) %(message)s')
+    stream_handler.setFormatter(f)
+    logger.addHandler(stream_handler)
+
+
+class TempModelListener(ModelListener):
+    def on_data_updated(self, data_type: DataType):
+        logger.info(f'on_data_update. {data_type}')
+
+    def on_buy_signal(self, code: str, qty: int):
+        logger.info(f'on_buy_signal. {code} {qty}')
+
+    def on_sell_signal(self, code: str, qty: int):
+        logger.info(f'on_sell_signal. {code} {qty}')
+
+
+class TestKiwoom(unittest.TestCase):
+    def setUp(self):
+        logger.info('setup')
+        self.app = QApplication(sys.argv)
+        self.tempWindow = QMainWindow()
+        self.tempModelListener = TempModelListener()
+        self.data_manager = DataManager()
+        self.data_manager.add_listener(self.tempModelListener)
+        self.kiwoom_manager = Kiwoom(self.data_manager)
+
+    def tearDown(self):
+        logger.info('tearDown')
+        self.app.exit()
+
+    def test_on_receive_chejan_data(self):
+        logger.info('test for test_on_receive_chejan_data()')
+
+        def temp_get_chejan_data(fid):
+            mock_dic = {
+                9201: '5323492810',
+                9001: 'A123420',
+                917: '00',
+                916: '00000000',
+                302: '선데이토즈',
+                10: '+12450',
+                930: '24',
+                931: '12448',
+                932: '298750',
+                933: '24',
+                945: '24',
+                946: '2',
+                950: '0',
+                951: '0',
+                27: '+12450',
+                28: '+12300',
+                307: '11800',
+                8019: '0.00',
+                957: '0',
+                958: '0',
+                918: '00000000',
+                990: '0',
+                991: '0.00',
+                992: '0',
+                993: '0.00',
+                959: '0',
+                924: '0',
+                10010: '-11750',
+                25: '2',
+                11: '+650',
+                12: '5.51',
+                306: '-8300',
+                305: '+15300',
+                970: '10',
+            }
+            return mock_dic[fid] if fid in mock_dic.keys() else ''
+
+        self.kiwoom_manager.ocx.get_chejan_data = Mock(side_effect=temp_get_chejan_data)
+        stock = self.data_manager.get_stock('123420')
+        stock.name = '선데이토즈'
+        stock.qty = 0
+
+        # when
+        self.kiwoom_manager.handler.on_receive_chejan_data('1', 34, '9201;9001;917;916;302;10;930;931;932;933;945;946;950;951;27;28;307;8019;957;958;918;990;991;992;993;959;924;10010;25;11;12;306;305;970')
+
+        # then
+        self.assertAlmostEqual(24, stock.qty)
+
