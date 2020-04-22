d2ea5981607ec016619c9dbe3995612e06dd1f69
sshim from hp <mechurak@gmail.com>
2020-04-23 07:32:43 +0900

    Introduce MINICONDA_PATH env variable

--- run_snstradebot.bat
+++ run_snstradebot.bat
@@ -1,7 +1,6 @@
 @echo on
-call C:\ProgramData\Miniconda3\Scripts\activate.bat
+call %MINICONDA_PATH%\Scripts\activate.bat
 :: bat 파일 위치
 cd "%~dp0\sns_trade_bot"
 set PYTHONPATH=%PYTHONPATH%;%~dp0
 python main.py
-pause
